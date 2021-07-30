52e56c8c93b727c20061a68a99b6094fcc9d8fc0
damirstash34 <62292195+damirstash34@users.noreply.github.com>
2021-07-30 16:52:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = ""
-GROUP_ID = 
+TOKEN = "1903563838:AAGPbpBt2Ww1nGugJYu-v79X-CYaqkaVpws"
+GROUP_ID = -1001519524581
 stoppedc = False
 helloIMG = "./hello.jpg"
-forReportId = 
+forReportId = 1097478693
